7bf13c490f068fb10a9847fdbcf19c4e7b939d2f
Samuel <samuelorlato@gmail.com>
2022-01-04 16:13:55 +0000

    Using Typer library

--- main.py
+++ main.py
@@ -3,29 +3,21 @@ import requests
 import smtplib
 import email.message as email
 
-URL = str(input("Coloque a URL do produto do Mercado Livre: "))
+import typer
 
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0 X-Middleton/1"}
+app = typer.Typer()
 
-site = requests.get(URL, headers = headers)
-
-soup = BeautifulSoup(site.content, "html.parser")
-
-product_title = soup.find("h1", class_ = "ui-pdp-title").get_text()
-
-product_price = float(soup.find("span", class_ = "price-tag-fraction").get_text().strip().replace(".", ""))
-
-def send_email():
+def send_email(URL, product_title, to_email, price, from_email, from_email_password):
     email_content = URL
 
     msg = email.Message()
 
-    msg["Subject"] = "Preço de " + str(product_title) + " abaixou!"
+    msg["Subject"] = "Preço de " + str(product_title) + " chegou a R$"+str(price)+"!"
 
-    msg["From"] = "emailfrom"
-    msg["To"] = str(input("Digite seu email: "))
+    msg["From"] = from_email
+    msg["To"] = to_email
     
-    password = "passwordfrom"
+    password = from_email_password
 
     msg.add_header("Content-Type", "text/html")
     msg.set_payload(email_content)
@@ -35,9 +27,28 @@ def send_email():
     s.login(msg["From"], password)
     s.sendmail(msg["From"], [msg["To"]], msg.as_string())
 
-    print("Email enviado!")
+    typer.echo(typer.style(f"Email enviado para {to_email}!", fg=typer.colors.GREEN, bold=True))
 
-limit = float(input("Digite o preço mínimo para receber notificações: R$"))
+@app.command()
+def track(to_email: str = typer.Option(...), product_url: str = typer.Option(...), target_price: float = typer.Option(...), from_email: str = typer.Option(...), from_email_password: str = typer.Option(...)):
+    to_email = to_email
 
-if product_price <= limit:
-    send_email()
+    URL = product_url
+
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0 X-Middleton/1"}
+
+    site = requests.get(URL, headers = headers)
+
+    soup = BeautifulSoup(site.content, "html.parser")
+
+    product_title = soup.find("h1", class_ = "ui-pdp-title").get_text()
+
+    product_price = float(soup.find("span", class_ = "price-tag-fraction").get_text().strip().replace(".", ""))
+
+    limit = target_price
+
+    if product_price <= limit:
+        send_email(URL=URL, product_title=product_title, to_email=to_email, price=product_price, from_email=from_email, from_email_password=from_email_password)
+
+if __name__ == "__main__":
+    app()
